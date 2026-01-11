d9319c56969efda3e06b437816a05285619db79c
Elvina Neila <elvinaneila0902@gmail.com>
2026-01-11 17:38:57 +0700

    struktur & route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Pasien Routes
+Route::get('/', fn() => view('auth.login'));
+
+Route::get('/pasien', fn() => view('pasien.dashboard'));
+Route::get('/pasien/riwayat', fn() => view('pasien.riwayat'));
+Route::get('/pasien/pemeriksaan', fn() => view('pasien.pemeriksaan'));
+Route::get('/pasien/artikel', fn() => view('pasien.artikel'));
+Route::get('/pasien/artikel/detail', fn() => view('pasien.artikel-detail'));
+
+
+// Admin Kepegawaian Routes
+Route::get('/kepegawaian', fn() => view('kepegawaian.dashboard'));
+Route::get('/kepegawaian/riwayat', fn() => view('kepegawaian.riwayat'));
+Route::get('/kepegawaian/pemeriksaan', fn() => view('kepegawaian.pemeriksaan'));
+Route::get('/kepegawaian/pegawai', fn() => view('kepegawaian.pegawai'));
+Route::get('/kepegawaian/pegawai/detail', fn() => view('kepegawaian.pegawai-detail'));
+Route::get('/kepegawaian/laporan', fn() => view('kepegawaian.laporan'));
+
